4741a39a04fe9554f3fcae0abb2dfc0539fc8b40
Christopher Kilian <cdkilian@gmail.com>
2020-06-18 15:14:23 -0700

    New front facing components added

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  // working note: in Javascript, declaring a variable with no prefix creates a new
+  // GLOBAL variable/property accessible via "window"
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in successfully');
+    }, error => {
+      console.log('FAILED to log in');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('Logged Out');
+  }
+
+}
