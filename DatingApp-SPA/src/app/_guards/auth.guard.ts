83936f8a1577c6fd1c054e898ed62450ee7960c7
Christopher Kilian <cdkilian@gmail.com>
2020-06-24 13:45:05 -0700

    Angular Routing added

---
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router, 
+              private alertify: AlertifyService) {}
+
+  canActivate(): boolean {
+    if (this.authService.loggedIn()) {
+      return true;
+    }
+
+    this.alertify.error('YOU SHALL NOT PASS!!!');
+    this.router.navigate(['/home']);
+    return false;
+  }
+  
+}
+
+
+// Note: This guard class works with Angular routing to ensure users must be logged in to access certain routes
---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,28 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+export const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent },
+            { path: 'messages', component: MessagesComponent },
+            { path: 'lists', component: ListsComponent }
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+];
+
+// NOTE: "Routes" above acts similar to switch statement in that it's fall-through
+// router looks for a matching path starting with home and ending with the wildcard
+// Means ordering is important!
+
+// This file referenced in app.module imports: RouterModule.forRoot(appRoutes)
+// see vid 67 for discussion of use of "dummy" path above (empty path with children for route guarding)
